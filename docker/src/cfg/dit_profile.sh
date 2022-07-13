d95832e6b29ec798855a87ed492dbfb2fece3ccd
inada tsukasa <invincibility3@icloud.com>
2022-07-14 02:31:54 +0900

    brush up sh-init-file

--- docker/src/cfg/dit_profile.sh
+++ docker/src/cfg/dit_profile.sh
@@ -1,29 +1,26 @@
-#!/bin/sh
-
 
 #
 # initialize files related Docker in shared directory
 #
 
-if ! pushd /dit; then
+if [ ! -e /dit ]; then
     echo "Please check if you created or specified the directory to be bound."
     exit
 fi
 
 
-if [ ! -e Dockerfile ]; then
-    touch Dockerfile
+if [ ! -e /dit/Dockerfile ]; then
+    touch /dit/Dockerfile
 fi
 
-if [ ! -e Dockerfile.draft ]; then
-    echo "FROM ${BASE}:${VERSION}" > Dockerfile.draft
-    echo "WORKDIR $(pwd)" >> Dockerfile.draft
+if [ ! -e /dit/Dockerfile.draft ]; then
+    (echo "FROM ${BASE}:${VERSION}" && echo "WORKDIR $(pwd)") > /dit/Dockerfile.draft
 fi
 unset BASE
 unset VERSION
 
-if [ ! -e .dockerignore ]; then
-    cat << EOF > .dockerignore
+if [ ! -e /dit/.dockerignore ]; then
+    cat << EOF > /dit/.dockerignore
 .cmd_history
 .ignore_hist
 .ignore_dock
@@ -37,15 +34,13 @@ fi
 # if necessary, reproduce the environment under construction
 #
 
-if [ ! -e .cmd_history ]; then
-    touch .cmd_history
+if [ ! -e /dit/.cmd_history ]; then
+    touch /dit/.cmd_history
 fi
 
-if [ -s .cmd_history ]; then
-    source .cmd_history > /dev/null
+if [ -s /dit/.cmd_history ]; then
+    . /dit/.cmd_history > /dev/null
 fi
-
-popd
 
 
 
@@ -53,14 +48,28 @@ popd
 # set PS1, to be referred every time any command is executed
 #
 
+CHANGE_HIST=+0
+CHANGE_DOCK=+0
+export CHANGE_HIST CHANGE_DOCK
+
+
+PROMPT_DIT(){
+    if [ $? ]; then
+        convert "$( history -1 | awk '{$1 = ""; sub(/[ \t]+/, "", $0); print $0}' )" \
+        | tee >( grep -A"${CHANGE_HIST}" '^ [To History]' | tail -"${CHANGE_HIST}" >> /dit/.cmd_history ) \
+        | grep -A"${CHANGE_DOCK}" '^ [To Dockerfile]' | tail -"${CHANGE_DOCK}" >> /dit/Dockerfile.draft
+    fi
+}
+
 PROMPT_OPT(){
     :
 }
 
 PROMPT_STR(){
-    echo '\u@\h:\w \$ '
+    echo "[hist:${CHANGE_HIST} dock:${CHANGE_DOCK}] \u@\h:\w \$ "
 }
 
-PS1='$( (reflect -n && PROMPT_OPT) > /dev/null )'"$( PROMPT_STR )"
+
+PS1='$( (PROMPT_DIT && PROMPT_OPT) > /dev/null && PROMPT_STR )'
 export PS1
 readonly PS1
