ef2518806e7fc168741d1bb6a5017625e0a325df
inada tsukasa <invincibility3@icloud.com>
2022-07-15 21:34:05 +0900

    complete sh-init-file except for convert part

--- docker/src/cfg/dit_profile.sh
+++ docker/src/cfg/dit_profile.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 
 #
 # initialize files related Docker in shared directory
 #
 
 if [ ! -e /dit ]; then
-    echo "Please check if you created or specified the directory to be bound."
-    exit
+    echo "Error: please check if you created or specified the directory to be bound." 1>&2
+    exit 1
 fi
 
 
@@ -14,7 +16,7 @@ if [ ! -e /dit/Dockerfile ]; then
 fi
 
 if [ ! -e /dit/Dockerfile.draft ]; then
-    (echo "FROM ${BASE}:${VERSION}" && echo "WORKDIR $(pwd)") > /dit/Dockerfile.draft
+    ( echo "FROM ${BASE}:${VERSION}"; echo "WORKDIR $(pwd)" ) > /dit/Dockerfile.draft
 fi
 unset BASE
 unset VERSION
@@ -22,8 +24,8 @@ unset VERSION
 if [ ! -e /dit/.dockerignore ]; then
     cat << EOF > /dit/.dockerignore
 .cmd_history
-.ignore_hist
-.ignore_dock
+.cmd_ignore.h
+.cmd_ignore.d
 user_entry.sh
 EOF
 fi
@@ -31,16 +33,24 @@ fi
 
 
 #
-# if necessary, reproduce the environment under construction
+# set environment variables, reserved by this tool
 #
 
-if [ ! -e /dit/.cmd_history ]; then
-    touch /dit/.cmd_history
-fi
+CHANGE_IN_HISTORY=+0
+CHANGE_IN_DOCKERFILE=+0
 
-if [ -s /dit/.cmd_history ]; then
-    . /dit/.cmd_history > /dev/null
-fi
+MODE_OF_REFLECT=both
+MODE_OF_IGNORE=simple
+
+MAXLEN_OF_COMMAND=128
+MAXLEN_OF_LINE=1024
+
+
+export \
+    LAST_EXIT_STATUS  LAST_SENTENCE \
+    CHANGE_IN_HISTORY  CHANGE_IN_DOCKERFILE \
+    MODE_OF_REFLECT  MODE_OF_IGNORE \
+    MAXLEN_OF_COMMAND  MAXLEN_OF_LINE
 
 
 
@@ -48,28 +58,44 @@ fi
 # set PS1, to be referred every time any command is executed
 #
 
-CHANGE_HIST=+0
-CHANGE_DOCK=+0
-export CHANGE_HIST CHANGE_DOCK
+PROMPT_REFLECT(){
+    [ $? = 0 ] && LAST_EXIT_STATUS=0 || LAST_EXIT_STATUS=1
+    LAST_SENTENCE="$( history 1 | awk 'NR == 1 { $1 = ""; sub(/[ \t]*/, "", $0) } { print $0 }' )"
 
-
-PROMPT_DIT(){
-    if [ $? ]; then
-        convert "$( history -1 | awk '{$1 = ""; sub(/[ \t]+/, "", $0); print $0}' )" \
-        | tee >( grep -A"${CHANGE_HIST}" '^ [To History]' | tail -"${CHANGE_HIST}" >> /dit/.cmd_history ) \
-        | grep -A"${CHANGE_DOCK}" '^ [To Dockerfile]' | tail -"${CHANGE_DOCK}" >> /dit/Dockerfile.draft
+    if [ "${LAST_EXIT_STATUS}" = 0 ]; then
+        :
     fi
 }
 
-PROMPT_OPT(){
+PROMPT_OPTION(){
     :
 }
 
-PROMPT_STR(){
-    echo "[hist:${CHANGE_HIST} dock:${CHANGE_DOCK}] \u@\h:\w \$ "
+PROMPT_REPORT(){
+    echo "[hist:${CHANGE_IN_HISTORY%:*} dock:${CHANGE_IN_DOCKERFILE%:*}] "
 }
 
+export -f PROMPT_REFLECT PROMPT_OPTION PROMPT_REPORT || true
 
-PS1='$( (PROMPT_DIT && PROMPT_OPT) > /dev/null && PROMPT_STR )'
+
+PROMPT_STRING='\u@\h:\w \$ '
+export PROMPT_STRING
+
+
+PS1='$( (PROMPT_REFLECT && PROMPT_OPTION) > /dev/null && PROMPT_REPORT )'"${PROMPT_STRING:-\\$ }"
 export PS1
 readonly PS1
+
+
+
+#
+# if necessary, reproduce the environment under construction
+#
+
+if [ -e /dit/.cmd_history ]; then
+    if [ -s /dit/.cmd_history ]; then
+        . /dit/.cmd_history > /dev/null
+    fi
+else
+    touch /dit/.cmd_history
+fi
