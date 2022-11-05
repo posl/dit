e23fe29e474da8b3d5b626a989f1e61d9bffc005
inada tsukasa <invincibility3@icloud.com>
2022-11-06 00:45:53 +0900

    changed to record the package manager used

--- docker/etc/dit_install.sh
+++ docker/etc/dit_install.sh
@@ -2,18 +2,24 @@
 
 
 if ( apt-get --version > /dev/null 2>&1 ); then
+    readonly PACKAGE_MANAGER='apt-get'
+
     apt-get update
     apt-get install -y --no-install-recommends "$@"
     apt-get clean
     rm -rf /var/lib/apt/lists/*
 
 elif ( yum --version > /dev/null 2>&1 ); then
+    readonly PACKAGE_MANAGER='yum'
+
     yum update -y
     yum install -y "$@"
     yum clean all
     rm -rf /var/cache/yum
 
 elif ( apk --version > /dev/null 2>&1 ); then
+    readonly PACKAGE_MANAGER='apk'
+
     apk update
     apk add --no-cache "$@"
 
@@ -22,3 +28,5 @@ else
     exit 1
 
 fi
+
+echo "${PACKAGE_MANAGER}" > /dit/etc/package_manager
