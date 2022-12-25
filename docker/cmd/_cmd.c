babb2b1c6ff6420807030a35d82d95d81c2d436c
inada tsukasa <invincibility3@icloud.com>
2022-12-25 18:13:05 +0900

    decided not to implement this subcommand at the time of writing the graduation thesis

--- docker/cmd/_cmd.c
+++ docker/cmd/_cmd.c
@@ -1,36 +1,9 @@
-/**
- * @file _cmd.c
- *
- * Copyright (c) 2022 Tsukasa Inada
- *
- * @brief Described the dit command 'cmd', 
- * @author Tsukasa Inada
- * @date 
- *
- * @note 
- */
-
 #include "main.h"
 
 #define CMD_FILE "/dit/var/cmd.log"
 
 
 
-
-/******************************************************************************
-    * Local Main Interface
-******************************************************************************/
-
-
-/**
- * @brief 
- *
- * @param[in]  argc  the number of command line arguments
- * @param[out] argv  array of strings that are command line arguments
- * @return int  command's exit status
- *
- * @note treated like a normal main function.
- */
 int cmd(int argc, char **argv){
     fputs("cmd\n", stdout);
     return 0;
