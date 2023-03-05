885c1e279a90929b171e15701963f9525f7f391b
inada tsukasa <invincibility3@icloud.com>
2023-03-06 08:48:34 +0900

    completed except for the part that needs golang 'Glob' function

--- docker/cmd/srcglob.c
+++ docker/cmd/srcglob.c
@@ -28,7 +28,8 @@
 
 #include <sys/types.h>
 
-#include <glob.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -38,14 +39,18 @@
 #define SUCCESS 0
 #define FAILURE 1
 
-#ifndef GLOB_PERIOD
-#define GLOB_PERIOD 0
-#endif
+#define ERR_STANDARD  1
+#define ERR_OTHERWISE 2
 
-#define DIT_MOUNT_DIR "/dit/mnt"
+#define MOUNT_DIR_LEN 8
+#define MOUNT_DIR_PATH "/dit/mnt"
 
-#define OUTPUT_FORMAT_REG  (DIT_MOUNT_DIR "%s\n")
-#define OUTPUT_FORMAT_DIR  (DIT_MOUNT_DIR "%s/\n")
+
+/** Data type for storing the error information */
+typedef struct {
+    const char *msg;     /** error message */
+    const char *info;    /** additional information, if any */
+} srcglob_err;
 
 
 /** Data type that is applied to the smallest element that makes up the results of this command */
@@ -53,20 +58,25 @@ typedef struct {
     char *path;    /** real path */
     bool isdir;    /** whether this is a directory */
     size_t len;    /** the size of real path */
-} src_data;
+} srcglob_data;
 
 
-static void xperror_message(const char *msg, const char *addition);
+static int do_srcglob(int argc, char **argv);
 
-static void report_srcglob_err(int type, int code, const char *entity);
-static int report_standard_err(const char *entity, int errid);
+static int resolve_src_paths(srcglob_data *src_array, srcglob_info *result);
+static void report_src_paths(srcglob_data *src_array, const srcglob_info *result);
+
+static void xperror_message(int type, srcglob_err *err);
 
 
-/** string representing a invoked command name */
+/** string representing an invoked command name */
 static const char *program_name;
 
-/** a handler for the binary file that records the results of this command */
-static FILE *srcglob_fp = NULL;
+/** descriptor for the temporary file to receive the results of my golang library being called */
+static int fd_buf = -1;
+
+/** handler for the data file to store the results of this command */
+static FILE *fp_dat = NULL;
 
 
 
@@ -79,172 +89,111 @@ static FILE *srcglob_fp = NULL;
 /**
  * @brief the extra command 'srcglob'
  *
- * @param[in]  argc  the length of the argument vector below
- * @param[out] argv  array of strings that are wildcard patterns of COPY/ADD instruction
+ * @param[in]  argc  the number of command line arguments
+ * @param[in]  argv  array of strings that are command line arguments
  * @return int  command's exit status
+ *
+ * @note before executing 'chroot', it is necessary to call functions that need to resolve the file path.
+ * @note opened files are closed at the end because it is a problem if 'errno' is overwritten.
  */
 int main(int argc, char **argv){
-    int errtype = '\0', code = -1, exit_status = FAILURE;
-    const char *errinfo = NULL;
+    int exit_status = ERR_STANDARD;
+    srcglob_err err = {0};
 
-    bool glob_done = false;
-    glob_t pglob;
-
-    if ((argc <= 0) || (! (argv && (program_name = *argv))))
-        goto exit;
+    if ((argc <= 0) || (! (argv && (program_name = *(argv++)))))
+        return -1;
 
     if (! --argc){
-        errtype = 'M';
-        errinfo = "requires one or more arguments";
+        exit_status = ERR_OTHERWISE;
+        err.msg = "requires one or more arguments";
         goto exit;
     }
-    if (! (srcglob_fp = fopen(SRCGLOB_FILE, "wb"))){
-        errtype = 'M';
-        errinfo = "cannot record the results";
+    if ((fd_buf = open(SRCGLOB_FILE_B, (O_RDWR | O_CREAT | O_TRUNC), (S_IRUSR | S_IWUSR))) == -1){
+        err.info = SRCGLOB_FILE_B;
+        goto exit;
+    }
+    if (! (fp_dat = fopen(SRCGLOB_FILE_D, "wb"))){
+        err.info = SRCGLOB_FILE_D;
         goto exit;
     }
 
-    if (geteuid()){
-        errtype = 'O';
-        code = SRCGLOB_NO_PRIVILEGE;
-        goto report;
+    if ((exit_status = do_srcglob(argc, argv)) == ERR_OTHERWISE){
+        exit_status = ERR_STANDARD;
+        err.info = MOUNT_DIR_PATH;
     }
-    if (chdir(DIT_MOUNT_DIR)){
-        errtype = 'S';
-        code = errno;
-        errinfo = DIT_MOUNT_DIR;
-        goto report;
-    }
-    if (chroot(".")){
-        errtype = 'S';
-        code = errno;
-        goto report;
-    }
-
-
-    // inconsistent with COPY instruction if GNU extension to 'glob' function is not available
-    code = GLOB_PERIOD;
-    assert(code);
-
-    do {
-        if (! *(++argv))
-            goto free;
-
-        glob_done = true;
-
-        switch (glob(*argv, code, report_standard_err, &pglob)){
-            case 0:
-                break;
-            case GLOB_NOMATCH:
-                errtype = 'O';
-                code = SRCGLOB_NOTHING_MATCHED;
-                errinfo = *argv;
-            default:
-                goto report;
-        }
-        code = GLOB_PERIOD | GLOB_APPEND;
-    } while (--argc);
-
-    assert(pglob.gl_pathc > 0);
-    assert(pglob.gl_pathv && *(pglob.gl_pathv));
-
-
-    src_data *src_array = NULL, *p_src, *p_dest;
-    char * const *p_path;
-    srcglob_info result = {0};
-    struct stat file_stat;
-    const char *format = OUTPUT_FORMAT_REG;
-
-    if ((src_array = (src_data *) malloc(sizeof(src_data) * pglob.gl_pathc))){
-        for (p_src = src_array, p_path = pglob.gl_pathv; *p_path; p_src++, p_path++){
-            if ((p_src->path = realpath(*p_path, NULL))){
-                errno = 0;
-                result.total_num++;
-
-                if (! lstat(p_src->path, &file_stat)){
-                    if ((p_src->isdir = S_ISDIR(file_stat.st_mode)))
-                        result.dirs_num++;
-
-                    if (p_src->isdir || S_ISREG(file_stat.st_mode)){
-                        p_src->len = strlen(p_src->path) + 1;
-                        result.written_size += p_src->len + 8;  // add the length of "/dit/mnt"
-                        continue;
-                    }
-                }
-            }
-
-            errtype = 'O';
-            code = errno;
-            errinfo = *p_path;
-
-            switch (code){
-                case ENOENT:
-                    code = SRCGLOB_NOT_IN_CONTEXT;
-                    break;
-                case 0:
-                    code = SRCGLOB_IS_SPECIAL_FILE;
-                    break;
-                default:
-                    errtype = 'S';
-            }
-            break;
-        }
-
-        if (! errtype){
-            exit_status = SUCCESS;
-            assert(result.total_num == pglob.gl_pathc);
-            fwrite(&result, sizeof(result), 1, srcglob_fp);
-        }
-
-        assert(result.total_num || (! result.dirs_num));
-
-        do {
-            if (! result.total_num){
-                if (! result.dirs_num)
-                    break;
-                result.total_num = result.dirs_num;
-                result.dirs_num = 0;
-                format = OUTPUT_FORMAT_DIR;
-            }
-            for (p_src = (p_dest = src_array); result.total_num; p_src++, result.total_num--){
-                if (! errtype){
-                    if (result.dirs_num && p_src->isdir){
-                        if (p_src != p_dest)
-                            memcpy(p_dest, p_src, sizeof(src_data));
-                        p_dest++;
-                        continue;
-                    }
-                    fputs(DIT_MOUNT_DIR, srcglob_fp);
-                    fwrite(p_src->path, sizeof(char), p_src->len, srcglob_fp);
-                    fprintf(stdout, format, p_src->path);
-                }
-                free(p_src->path);
-            }
-        } while (! errtype);
-
-        free(src_array);
-    }
-
-
-report:
-    if (errtype){
-        report_srcglob_err(errtype, code, errinfo);
-        errtype = '\0';
-    }
-
-free:
-    if (glob_done)
-        globfree(&pglob);
-
-    assert(srcglob_fp);
-    fclose(srcglob_fp);
 
 exit:
-    if (errtype){
-        assert(errtype == 'M');
-        xperror_message(errinfo, NULL);
+    switch (exit_status){
+        case SUCCESS:
+            break;
+        case ERR_STANDARD:
+        case ERR_OTHERWISE:
+            xperror_message(exit_status, &err);
+        default:
+            exit_status = FAILURE;
     }
 
+    if (fd_buf != -1)
+        close(fd_buf);
+    if (fp_dat)
+        fclose(fp_dat);
+
+    return exit_status;
+}
+
+
+
+
+/**
+ * @brief perform source path expansion.
+ *
+ * @param[in]  argc  the length of the argument vector below
+ * @param[in ] argv  array of strings that are wildcard patterns of COPY/ADD instruction
+ * @return int  -1 (failure), 0 (success), 1 (standard error) or 2 (unexpected error)
+ *
+ * @note if the return value is 2, you should print a standard error message with additional information.
+ */
+static int do_srcglob(int argc, char **argv){
+    assert(argc > 0);
+    assert(argv);
+
+    if (chdir(MOUNT_DIR_PATH))
+        return ERR_OTHERWISE;
+
+    if (chroot("."))
+        return ERR_STANDARD;
+
+    srcglob_info result = {0};
+    int exit_status = -1;
+
+    /*
+    if (! (result.total_num = MyGlob(program_name, argc, argv, fd_buf)))
+        return exit_status;
+    */
+//
+    result.total_num = 3;
+    write(fd_buf, "/Dockerfile", 12);
+    write(fd_buf, "../tmp/", 8);
+    write(fd_buf, "./.dockerignore", 16);
+//
+
+    srcglob_data src_array[result.total_num];
+    memset(src_array, 0, (sizeof(srcglob_data) * result.total_num));
+
+    switch ((exit_status = resolve_src_paths(src_array, &result))){
+        case SUCCESS:
+            report_src_paths(src_array, &result);
+            break;
+        case ERR_OTHERWISE:
+            exit_status = -1;
+            break;
+        case -1:
+            for (srcglob_data *p_src = src_array; result.total_num; p_src++, result.total_num--)
+                if (p_src->path)
+                    free(p_src->path);
+    }
+
+    assert(exit_status != ERR_OTHERWISE);
     return exit_status;
 }
 
@@ -257,77 +206,178 @@ exit:
 
 
 /**
+ * @brief construct the data that are the results of source path expansion.
+ *
+ * @param[out] src_array  array for storing the results of source path expansion
+ * @param[out] result  variable to store the information about the results of source path expansion
+ * @return int  -1 (failure), 0 (success), 1 (standard error) or 2 (unexpected error)
+ *
+ * @note after calling my golang library, the file position of the buffer is set at the end of the file.
+ * @note if the return value is 1, you should convey the fact that an error has occurred through a message.
+ *
+ * @attention each element of 'src_array' and 'result' must be initialized properly before the call.
+ * @attention the absolute paths stored in 'src_array' should be freed by the caller when no longer needed.
+ */
+static int resolve_src_paths(srcglob_data *src_array, srcglob_info *result){
+    assert(src_array);
+    assert(result);
+    assert(result->total_num);
+
+    off_t file_size;
+    void *addr;
+
+    if ((file_size = lseek(fd_buf, 0, SEEK_CUR)) == -1)
+        return ERR_STANDARD;
+
+    if ((file_size <= 1) || lseek(fd_buf, 0, SEEK_SET))
+        return ERR_OTHERWISE;
+
+    if ((addr = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd_buf, 0)) == MAP_FAILED)
+        return ERR_STANDARD;
+
+    srcglob_data *p_src;
+    size_t srcs_num;
+    char *path;
+    struct stat file_stat;
+    int exit_status = SUCCESS;
+    srcglob_err err;
+
+    p_src = src_array;
+    srcs_num = result->total_num;
+    path = (char *) addr;
+
+    do {
+        if ((p_src->path = realpath(path, NULL))){
+            errno = 0;
+
+            if (! lstat(p_src->path, &file_stat))
+                switch ((file_stat.st_mode & S_IFMT)){
+                    case S_IFDIR:
+                        p_src->isdir = true;
+                        result->dirs_num++;
+                    case S_IFREG:
+                        p_src->len = strlen(p_src->path) + 1;
+                        result->written_size += p_src->len + MOUNT_DIR_LEN;
+                        goto next;
+                }
+        }
+
+        exit_status = ERR_OTHERWISE;
+        err.info = path;
+
+        switch (errno){
+            case 0:
+                err.msg = "is a special file";
+                break;
+            case ENOENT:
+                err.msg = "not in build context";
+                break;
+            default:
+                exit_status = ERR_STANDARD;
+        }
+        xperror_message(exit_status, &err);
+
+    next:
+        p_src++;
+        while (*(path++));
+    } while (--srcs_num);
+
+    if (exit_status)
+        exit_status = -1;
+
+    munmap(addr, file_size);
+
+    assert((! exit_status) || (exit_status == -1));
+    return exit_status;
+}
+
+
+
+
+/**
+ * @brief report the results of source path expansion.
+ *
+ * @param[out] src_array  array for storing the results of source path expansion
+ * @param[in]  result  variable to store the information about the results of source path expansion
+ *
+ * @note the results are reported in the order of regular files and directories.
+ * @note the absolute paths stored in 'src_array' are freed in this function.
+ */
+static void report_src_paths(srcglob_data *src_array, const srcglob_info *result){
+    assert(src_array);
+    assert(result);
+    assert(result->total_num);
+    assert(result->written_size);
+
+    char *path, addr[result->written_size];
+    size_t total_num, dirs_num;
+    const char *attr = "regular";
+    int offset = 1;
+    srcglob_data *p_src, *p_dest;
+
+    path = addr;
+    total_num = result->total_num;
+    dirs_num = result->dirs_num;
+
+    if (total_num == dirs_num)
+        total_num = 0;
+
+    do {
+        if (! total_num){
+            if (! dirs_num)
+                break;
+            total_num = dirs_num;
+            dirs_num = 0;
+            attr = "directory";
+        }
+
+        assert((! offset) || (offset == 1));
+        fprintf(stdout, ("\n < %s >\n" + offset), attr);
+        offset = 0;
+
+        for (p_src = (p_dest = src_array); total_num; p_src++, total_num--){
+            if (dirs_num && p_src->isdir){
+                if (p_src != p_dest)
+                    memcpy(p_dest, p_src, sizeof(srcglob_data));
+                p_dest++;
+                continue;
+            }
+            memcpy(path, MOUNT_DIR_PATH, (sizeof(char) * MOUNT_DIR_LEN));
+            memcpy((path + MOUNT_DIR_LEN), p_src->path, (sizeof(char) * p_src->len));
+            free(p_src->path);
+
+            fputs(path, stdout);
+            fputc('\n', stdout);
+            path += p_src->len + MOUNT_DIR_LEN;
+        }
+    } while (true);
+
+    fwrite(result, sizeof(srcglob_info), 1, fp_dat);
+    fwrite(addr, sizeof(char), result->written_size, fp_dat);
+}
+
+
+
+
+/**
  * @brief print the specified error message to stderr.
  *
- * @param[in]  msg  the error message
- * @param[in]  addition  additional information, if any
+ * @param[in]  type  1 (standard error) or 2 (original error)
+ * @param[out] err  the error information
  */
-static void xperror_message(const char *msg, const char *addition){
-    assert(msg);
+static void xperror_message(int type, srcglob_err *err){
+    assert(err);
 
     int offset = 0;
 
-    if (! addition){
+    if (type == ERR_STANDARD)
+        err->msg = strerror(errno);
+
+    if (! err->info){
         offset = 4;
-        addition = msg;
+        err->info = err->msg;
     }
 
     assert(program_name);
-    fprintf(stderr, ("%s: %s: %s\n" + offset), program_name, addition, msg);
-}
-
-
-
-
-/**
- * @brief report any errors that occur while running this command.
- *
- * @param[in]  type  error type
- * @param[in]  code  error code
- * @param[in]  entity  the entity that caused the error or NULL
- *
- * @note see the data type 'srcglob_err' for details on the arguments of this function.
- */
-static void report_srcglob_err(int type, int code, const char *entity){
-    assert((type == 'S') || (type == 'O'));
-    assert(code >= 0);
-
-    srcglob_err errinfo = {
-        .type = type,
-        .code = code,
-        .written_size = 0
-    };
-
-    const char *errmsg;
-
-    if (entity)
-        errinfo.written_size = strlen(entity) + 1;
-
-    assert(srcglob_fp);
-    fwrite(&errinfo, sizeof(errinfo), 1, srcglob_fp);
-
-    if (errinfo.written_size){
-        assert(entity);
-        fwrite(entity, sizeof(const char), errinfo.written_size, srcglob_fp);
-    }
-
-    errmsg = (type == 'S') ? strerror(code) : srcglob_errmsgs[code];
-    xperror_message(errmsg, entity);
-}
-
-
-/**
- * @brief report the standard error represented by 'errno'.
- *
- * @param[in]  entity  the entity that caused the error or NULL
- * @param[in]  errid  error number
- * @return int  -1 (error exit)
- *
- * @note can be passed as 'errfunc' in glibc 'glob' function.
- */
-static int report_standard_err(const char *entity, int errid){
-    assert(errid);
-
-    report_srcglob_err('S', errid, entity);
-    return -1;
+    fprintf(stderr, ("%s: %s: %s\n" + offset), program_name, err->info, err->msg);
 }
--- docker/cmd/srcglob.h
+++ docker/cmd/srcglob.h
@@ -2,26 +2,9 @@
 #define DIT_COPY_SRCGLOB
 
 
-/******************************************************************************
-    * Macros
-******************************************************************************/
+#define SRCGLOB_FILE_B "/dit/tmp/srcglob.buf"
+#define SRCGLOB_FILE_D "/dit/tmp/srcglob.dat"
 
-#define SRCGLOB_FILE "/dit/tmp/srcglob.bin"
-
-
-#define SRCGLOB_ERRMSGS_NUM 4
-
-#define SRCGLOB_NO_PRIVILEGE     0
-#define SRCGLOB_NOTHING_MATCHED  1
-#define SRCGLOB_NOT_IN_CONTEXT   2
-#define SRCGLOB_IS_SPECIAL_FILE  3
-
-
-
-
-/******************************************************************************
-    * Data Types
-******************************************************************************/
 
 /** Data type for storing the information about the results of source path expansion */
 typedef struct {
@@ -29,29 +12,6 @@ typedef struct {
     size_t dirs_num;        /** the number of directories in copy sources */
     size_t written_size;    /** size of strings written to the rest of the binary file */
 } srcglob_info;
-
-
-/** Data type for storing the error information */
-typedef struct {
-    int type;               /** error type ('S' (standard) or 'O' (original)) */
-    int code;               /** error code ('errno' or 'errcode') */
-    size_t written_size;    /** size of a string written to the rest of the binary file */
-} srcglob_err;
-
-
-
-
-/******************************************************************************
-    * Array
-******************************************************************************/
-
-/** array of error messages represented by 'errcode' */
-static const char * const srcglob_errmsgs[SRCGLOB_ERRMSGS_NUM] = {
-    "had no privilege",
-    "matched nothing",
-    "not in build context",
-    "is a special file"
-};
 
 
 #endif // DIT_COPY_SRCGLOB
