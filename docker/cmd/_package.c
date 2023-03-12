1a87044e71c2c9b2d08c194cb441f087436ea281
inada tsukasa <invincibility3@icloud.com>
2023-03-12 20:45:34 +0900

    added the temporary code so that this tool can be started even in alpine

--- docker/cmd/_package.c
+++ docker/cmd/_package.c
@@ -28,8 +28,36 @@
  * @note treated like a normal main function.
  */
 int package(int argc, char **argv){
-    fputs("package\n", stdout);
-    return 0;
+    char *line;
+    int errid = 0;
+    bool first_line = true;
+
+    while (xfgets_for_loop("/dit/etc/package_manager", &line, &errid)){
+        if (! first_line)
+            errid = -1;
+
+        first_line = false;
+    }
+
+    if (line){
+        if (! (errid || strcmp(line, "apk"))){
+            char *cmd[] = { "apk", "add", "--no-cache", "bash", NULL };
+
+            if (! execute("/sbin/apk", cmd, 1)){
+                char *instr = "RUN apk add --no-cache bash";
+
+                if (! get_file_size(DIT_PROFILE)){
+                    fputs(instr, stdout);
+                    fputc('\n', stdout);
+                }
+                else
+                    reflect_to_dockerfile(&instr, 1, true, '\0');
+            }
+        }
+
+        free(line);
+    }
+    return SUCCESS;
 }
 
 
