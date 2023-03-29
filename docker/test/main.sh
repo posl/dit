6bfad9a306701e109644d01a7ae2af42fe388136
inada tsukasa <invincibility3@icloud.com>
2023-03-30 04:09:47 +0900

    added missing test

--- docker/test/main.sh
+++ docker/test/main.sh
@@ -2,10 +2,29 @@
 
 
 #
+# Test Functions
+#
+
+do_test_startup_error(){
+    : 'error: a subcommand to be invoked must be specified'
+    dit && exit 1
+    read -r REPLY
+
+    : 'error: an invalid command must not be specified.'
+    dit push && exit 1
+    read -r REPLY
+}
+
+
+
+#
 # run test codes for each dit command
 #
 
 if dit test; then
+    # a few visual tests
+    do_test_startup_error
+
     set +x
     shopt -s nullglob
 
